ae37ce12f9d26128b7ff0497f3db4436c4b7b0a4
Alexandra Miresan <amiresan@gmail.com>
2026-03-18 12:22:48 +0200

    Added IExchangeRepository
    Added exchange repository interface

---
+++ BankingAppTeamB/BankingAppTeamB/BankingAppTeamB/Repositories/IExchangeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using BankingAppTeamB.Models;
+
+namespace BankingAppTeamB.Repositories;
+
+public interface IExchangeRepository
+{
+    public ExchangeTransaction Add(ExchangeTransaction transaction);
+    public List<ExchangeTransaction> GetByUserId();
+    public List<RateAlert> GetActiveAlerts(int userId);
+    public List<RateAlert> GetAllActiveAlerts();
+    public RateAlert AddAlert(RateAlert alert);
+    public void DeleteAlert(int id);
+    public void MarkAlertTriggered(int id);
+    
+}
